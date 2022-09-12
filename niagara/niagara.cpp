89719e133bba69ec356c4cb76d4b430aea34f0ef
Haruto2017 <phwei2016@outlook.com>
2022-09-12 12:05:06 -0700

    Added glfw libraries

--- niagara/niagara.cpp
+++ niagara/niagara.cpp
@@ -1,9 +1,25 @@
 // niagara.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <stdio.h>
+#include <assert.h>
+#include <iostream>
+
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    //std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n";
+
+    int rc = glfwInit();
+    assert(rc);
+
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "niagara", 0, 0);
+    assert(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
 }
