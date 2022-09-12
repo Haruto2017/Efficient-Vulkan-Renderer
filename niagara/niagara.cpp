12a81f3ce08826037c9bebd9e97026579b57fb33
Haruto2017 <phwei2016@outlook.com>
2022-09-12 15:26:40 -0700

    Quickly goes through basic vulkan initialization procedures from instance to swapchain. Ignores a lot of details in spec & currently not a generic implementation.

--- niagara/niagara.cpp
+++ niagara/niagara.cpp
@@ -3,9 +3,146 @@
 
 #include <assert.h>
 #include <iostream>
+#include <vector>
 
 #include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 #include <vulkan/vulkan.h>
+
+#define VK_CHECK(call) \
+    do { \
+        VkResult result_ = call; \
+        assert(result_ == VK_SUCCESS); \
+    } while (0)
+
+VkPhysicalDevice pickPhysicalDevice(VkPhysicalDevice * physicalDevices, uint32_t physicalDeviceCount)
+{
+    for (uint32_t i = 0; i < physicalDeviceCount; ++i)
+    {
+        VkPhysicalDeviceProperties props;
+        vkGetPhysicalDeviceProperties(physicalDevices[i], &props);
+
+        if (props.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+        {
+            std::cout << "Picking discrete GPU " << props.deviceName << std::endl;
+            return physicalDevices[i];
+        }
+    }
+
+    if (physicalDeviceCount > 0)
+    {
+        VkPhysicalDeviceProperties props;
+        vkGetPhysicalDeviceProperties(physicalDevices[0], &props);
+
+        std::cout << "Picking fallback GPU " << props.deviceName << std::endl;
+        return physicalDevices[0];
+    }
+
+    return VK_NULL_HANDLE;
+}
+
+bool checkValidationLayerSupport(const std::vector<const char*>& validationLayers) {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+VkInstance createInstance()
+{
+    VkApplicationInfo appInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
+    appInfo.apiVersion = VK_API_VERSION_1_3;
+
+    VkInstanceCreateInfo createInfo = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
+    createInfo.pApplicationInfo = &appInfo;
+
+#ifdef _DEBUG
+    const std::vector<const char*> validationLayers =
+    {
+        "VK_LAYER_KHRONOS_validation"
+    };
+
+    assert(checkValidationLayerSupport(validationLayers));
+
+    createInfo.ppEnabledLayerNames = validationLayers.data();
+    createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+#endif
+
+    const std::vector<const char*> extensions =
+    {
+        VK_KHR_SURFACE_EXTENSION_NAME,
+#ifdef VK_USE_PLATFORM_WIN32_KHR
+        VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
+#endif
+    };
+
+    createInfo.ppEnabledExtensionNames = extensions.data();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+
+    VkInstance instance = 0;
+    VK_CHECK(vkCreateInstance(&createInfo, 0, &instance));
+    return instance;
+}
+
+VkDevice createDevice(VkInstance instance, VkPhysicalDevice physicalDevice, uint32_t* familyIndex)
+{
+    *familyIndex = 0;
+
+    float queuePriorities[] = { 1.f };
+
+    VkDeviceQueueCreateInfo queueInfo = { VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO };
+    queueInfo.queueFamilyIndex = *familyIndex;
+    queueInfo.queueCount = 1;
+    queueInfo.pQueuePriorities = queuePriorities;
+
+    const std::vector<const char*> extensions =
+    {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
+    VkDeviceCreateInfo createInfo = { VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO };
+    createInfo.queueCreateInfoCount = 1;
+    createInfo.pQueueCreateInfos = &queueInfo;
+    createInfo.ppEnabledExtensionNames = extensions.data();
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+
+    VkDevice device = 0;
+    VK_CHECK(vkCreateDevice(physicalDevice, &createInfo, 0, &device));
+    return device;
+}
+
+VkSurfaceKHR createSurface(VkInstance instance, GLFWwindow* window)
+{
+#if defined(VK_USE_PLATFORM_WIN32_KHR)
+    VkWin32SurfaceCreateInfoKHR createInfo = { VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR };
+    createInfo.hinstance = GetModuleHandle(0);
+    createInfo.hwnd = glfwGetWin32Window(window);
+
+    VkSurfaceKHR surface = 0;
+    vkCreateWin32SurfaceKHR(instance, &createInfo, 0, &surface);
+    return surface;
+#else
+#error Unsupported platform
+#endif
+}
 
 int main()
 {
@@ -14,8 +151,31 @@ int main()
     int rc = glfwInit();
     assert(rc);
 
+    VkPhysicalDevice physicalDevices[16];
+    uint32_t physicalDeviceCount = sizeof(physicalDevices) / sizeof(physicalDevices[0]);
+
+    VkInstance instance = createInstance();
+    assert(instance);
+
+    VK_CHECK(vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDevices));
+
+    VkPhysicalDevice physicalDevice = pickPhysicalDevice(physicalDevices, physicalDeviceCount);
+    assert(physicalDevice);
+
+    uint32_t familyIndex = 0;
+    VkDevice device = createDevice(instance, physicalDevice, &familyIndex);
+    assert(device);
+
     GLFWwindow* window = glfwCreateWindow(1024, 768, "niagara", 0, 0);
     assert(window);
+
+    VkSurfaceKHR surface = createSurface(instance, window);
+    assert(surface);
+
+    //VkSwapchainCreateInfoKHR createInfo = { VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR };
+
+    //VkSwapchainKHR swapchain = 0;
+    //vkCreateSwapchainKHR(device, &createInfo, 0, &swapchain);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -23,4 +183,6 @@ int main()
     }
 
     glfwDestroyWindow(window);
+
+    vkDestroyInstance(instance, 0);
 }
